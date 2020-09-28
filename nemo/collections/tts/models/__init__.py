b496d06b476b91644ef6ee808e599f6215a989a2
Stanislav Beliaev <stasbelyaev96@gmail.com>
2020-09-28 16:04:47 +0300

    TalkNet durs and spect predictors.
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>

--- nemo/collections/tts/models/__init__.py
+++ nemo/collections/tts/models/__init__.py
@@ -15,6 +15,14 @@
 from nemo.collections.tts.models.glow_tts import GlowTTSModel
 from nemo.collections.tts.models.squeezewave import SqueezeWaveModel
 from nemo.collections.tts.models.tacotron2 import Tacotron2Model
+from nemo.collections.tts.models.talknet import TalkNetDursModel, TalkNetSpectModel
 from nemo.collections.tts.models.waveglow import WaveGlowModel
 
-__all__ = ["GlowTTSModel", "SqueezeWaveModel", "Tacotron2Model", "WaveGlowModel"]
+__all__ = [
+    "GlowTTSModel",
+    "SqueezeWaveModel",
+    "Tacotron2Model",
+    "WaveGlowModel",
+    "TalkNetDursModel",
+    "TalkNetSpectModel",
+]
